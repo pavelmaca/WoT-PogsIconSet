12fe3ac38f1348b1a12a1dd5e0060d193379f0ed
Pavel Máca <maca.pavel@gmail.com>
2015-12-18 23:42:55 +0100

    layers for MAX, FSR, VR, RLD versions

--- Layers/Armor.cs
+++ Layers/Armor.cs
@@ -11,13 +11,15 @@ namespace WotPogsIconSet.Layers
 {
     public class Armor
     {
+        const int MAX_WIDTH = 40;
+
         public static Layer TurretArmorFSR = (Graphics g, TankStats tankStats) =>
         {
             if (!tankStats.IsTurretInternal)
             {
                 string text = tankStats.TurretFront + "*" + tankStats.TurretSides + "*" + tankStats.TurretBack;
 
-                TextHelpers.helperDrawFontDinamic(g, text, Basic.BRUSH_WHITE, 79, 10, 67, FontAlign.Right);
+                TextHelpers.helperDrawFontDinamic(g, text, Basic.BRUSH_WHITE, 79, 10, MAX_WIDTH, FontAlign.Right);
             }
         };
 
@@ -27,8 +29,23 @@ namespace WotPogsIconSet.Layers
             {
                 string text = tankStats.TurretBack + "*" + tankStats.TurretSides + "*" + tankStats.TurretFront;
 
-                TextHelpers.helperDrawFontDinamic(g, text, Basic.BRUSH_WHITE, 79, 10, 67, FontAlign.Right);
+                TextHelpers.helperDrawFontDinamic(g, text, Basic.BRUSH_WHITE, 79, 10, MAX_WIDTH, FontAlign.Right);
             }
         };
+
+        public static Layer HullArmorFSR = (Graphics g, TankStats tankStats) =>
+        {
+
+            string text = tankStats.HullFront + "*" + tankStats.HullSides + "*" + tankStats.HullBack;
+            TextHelpers.helperDrawFontDinamic(g, text, Basic.BRUSH_WHITE, 79, 17, MAX_WIDTH, FontAlign.Right);
+        };
+
+        public static Layer HullArmorRSF = (Graphics g, TankStats tankStats) =>
+        {
+            string text = tankStats.HullBack + "*" + tankStats.HullSides + "*" + tankStats.HullFront;
+            TextHelpers.helperDrawFontDinamic(g, text, Basic.BRUSH_WHITE, 79, 17, MAX_WIDTH, FontAlign.Right);
+        };
+
+
     }
 }
--- Layers/Basic.cs
+++ Layers/Basic.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,14 +30,131 @@ namespace WotPogsIconSet.Layers
 
         public static Layer TankName = (Graphics g, TankStats tankStats) =>
         {
-            TextHelpers.helperDrawFont4px(g, ShortNames.findShortName(tankStats), BRUSH_WHITE, 19, 2);
+            String shortName = ShortNames.findShortName(tankStats);
+
+            if( PogsFontRenderer.getTextWidth(PogsFonts.font4, shortName) > 41)
+            {
+                Console.WriteLine("Name is too long:" + shortName);
+                Console.WriteLine("Press ENTER to continue:");
+                Console.ReadLine();
+            }
+            TextHelpers.helperDrawFont4px(g, shortName, BRUSH_WHITE, 19, 2);
         };
 
-        public static Layer Shield = (Graphics g, TankStats tankStat) =>
+        public static Layer Shield = (Graphics g, TankStats tankStats) =>
         {
-            using (Image shield = ImageTools.loadFromFile(Properties.Settings.Default.imagesLocation + String.Format(@"\sheilds\{0}.png", tankStat.Nation)))
+            using (Image shield = ImageTools.loadFromFile(Properties.Settings.Default.imagesLocation + String.Format(@"\sheilds\{0}.png", tankStats.Nation)))
                 g.DrawImageUnscaled(shield, 1, 2);
         };
+
+        public static Layer Premium = (Graphics g, TankStats tankStats) =>
+        {
+            if (tankStats.IsPremium)
+            {
+                g.DrawImageUnscaled(premiumStar, 4, 14);
+            }
+        };
+
+        public static Layer Tier = (Graphics g, TankStats tankStats) =>
+        {
+            TextHelpers.helperDrawFontNumbers(g, tankStats.Tier, BRUSH_WHITE, 9, 5, FontAlign.Center);
+        };
+
+        public static Layer ViewRange = (Graphics g, TankStats tankStats) =>
+        {
+            //this.DrawString(String.Format("{0:0.#}", tankStats.Hp), PogsFonts.font4, Pogs.colorOrange, x, y, FontAlign.Right);
+            TextHelpers.helperDrawFont4px(g, tankStats.ViewRange.ToString(), BRUSH_ORANGE, 37, 17, FontAlign.Right);
+        };
+
+        public static Layer HealPoints = (Graphics g, TankStats tankStats) =>
+        {
+            TextHelpers.helperDrawFont4px(g, tankStats.Hp.ToString(), BRUSH_ORANGE, 37, 17, FontAlign.Right);
+        };
+
+        public static Layer ReloadTime = (Graphics g, TankStats tankStats) =>
+        {
+            float reloadTime = tankStats.IsUsingHe ? tankStats.HeGun.ReloadTime : tankStats.ApGun.ReloadTime;
+            TextHelpers.helperDrawFont4px(g, String.Format("{0:0.#}", reloadTime), BRUSH_GOLD, 37, 10, FontAlign.Right);
+        };
+
+        public static Layer PenetrationOrDamage = (Graphics g, TankStats tankStats) =>
+        {
+            if (tankStats.Type == TankType.Spg)
+            {
+                PenetrationHelper(g, tankStats);
+            }
+            else
+            {
+                DamageHelper(g, tankStats);
+            }
+
+            //TextHelpers.helperDrawFont4px(g, String.Format("{0:0.#}", tankStats.Hp), BRUSH_ORANGE, 37, 17, FontAlign.Right);
+        };
+
+        public static Layer ContourIcon = (Graphics g, TankStats tankStats) =>
+        {
+            using (Image original = ImageTools.loadFromFile(Path.Combine(Properties.Settings.Default.contourLocation, tankStats.FileName)))
+            {
+                if (original == null)
+                {
+                    Console.WriteLine("No contour image for tank:" + tankStats.Name+"\n");
+                    return;
+                }
+
+                // make original image darker
+                ImageTools.darkenImg(original, 0.2f);
+
+                // resize original image, when its bigger then availible space
+                float scale = original.Width / (float)original.Height; // s = w / h
+
+                int maxWidth = 62;
+                int maxHeight = 15;
+
+                int newWidth = original.Width;
+                int newHeight = original.Height;
+
+                if (newWidth > maxWidth)
+                {
+                    newWidth = maxWidth;
+                    newHeight = (int)(newWidth / scale); // h = w / s
+                }
+
+                if (newHeight > maxHeight)
+                {
+                    newHeight = maxHeight;
+                    newWidth = (int)(newHeight * scale);  // w = h * s
+                }
+
+                // put scaled conture image at "x = 18" and "y = y.MAX - newHeight + 1" (+1 for cut out bottom transparent pixels)
+                g.DrawImage(original, 18, Icon.HEIGHT - newHeight + 1, newWidth, newHeight);
+
+            }
+        };
+
+
+        // Setup
+
+        protected static Image premiumStar;
+        protected static Dictionary<TankType, Color[]> bgColors = new Dictionary<TankType, Color[]>(5);
+
+        static Basic()
+        {
+
+            premiumStar = ImageTools.loadFromFile(Path.Combine(Properties.Settings.Default.imagesLocation, "star.png"));
+        }
+
+        protected static void PenetrationHelper(Graphics g, TankStats tankStats)
+        {
+
+            int text = tankStats.IsUsingHe ? tankStats.HeGun.HePenetration : tankStats.ApGun.ApPenetration; 
+            TextHelpers.helperDrawFont4px(g, text.ToString(), BRUSH_ORANGE, 79, 2, FontAlign.Right);
+        }
+
+        protected static void DamageHelper(Graphics g, TankStats tankStats)
+        {
+            int text = tankStats.IsUsingHe ? tankStats.HeGun.HeDamage : tankStats.ApGun.ApDamage;
+            TextHelpers.helperDrawFont4px(g, text.ToString(), BRUSH_ORANGE, 79, 2, FontAlign.Right);
+        }
 
     }
 }

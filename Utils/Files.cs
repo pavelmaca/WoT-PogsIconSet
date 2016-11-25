8988b21e5721d97ee08f2eca4d45ec86ec768782
Pavel Máca <maca.pavel@gmail.com>
2016-11-25 20:31:59 +0100

    add missing source Utils/Files.cs

---
+++ Utils/Files.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WotPogsIconSet.Utils
+{
+    public class Files
+    {
+
+        public static void deleteDirectoryRecusivly(String path)
+        {
+            Console.WriteLine("Are you sure to delete " + path + " ?");
+            Console.ReadLine();
+            System.IO.DirectoryInfo di = new DirectoryInfo(path);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+
+        }
+    }
+}
